d3a112b1df964b9e288d97f3773f5d4ce49d1084
蒋伟 <872505975@qq.com>
2016-05-03 17:12:25 +0800

    first commit

---
+++ server/createdb.js
@@ -0,0 +1,9 @@
+var server = require('./server');
+var ds = server.dataSources.mysql;
+var lbTables = ['Member', 'Token','Journalism'];
+//var lbTables = ['Journalism'];
+ds.automigrate(lbTables, function(er) {
+  if (er) throw er;
+  console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
+  ds.disconnect();
+});
